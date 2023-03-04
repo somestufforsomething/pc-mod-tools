c0f6bc28e092ba800da153cf41ef27844da63dc1
somestufforsomething <96510751+somestufforsomething@users.noreply.github.com>
2023-03-03 22:12:33 -0500

    update filter

--- YouTube Chat Regex Mod Actions.user.js
+++ YouTube Chat Regex Mod Actions.user.js
@@ -8,7 +8,7 @@
 // @license          MIT
 // @match            https://youtube.com/*
 // @match            https://*.youtube.com/*
-// @version          20230202.3
+// @version          20230303.1
 // ==/UserScript==
 
 // ======================== Settings ============================
@@ -129,7 +129,7 @@ const msg_filter = [
 const del_filter = [
     /exoverse/i,
     /It's only the cold wind I feelIt's me that I spite as I stand up and fightThe only thing I know for realThere will be blood \(blood\) shed \(shed\)The man in the mirror nods his head/i,
-    /ttd/i,
+    /\bttd\b/i,
 ];
 
 // TODO: add which action {del, time, hide} to perform:
